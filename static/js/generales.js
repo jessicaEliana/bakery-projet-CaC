1a6c2ac9a2bda3561df3cc4c1a24410bf657b427
AndySaav <84095104+AndySaav@users.noreply.github.com>
2024-05-21 01:48:43 -0300

    envio cambios en las secciones de cada producto y nombres en elmenu

--- static/js/generales.js
+++ static/js/generales.js
@@ -13,3 +13,37 @@ const verScroll = function() {
 }
 
 window.addEventListener('scroll', verScroll )
+
+// onclick 
+
+document.getElementById('icon-home').onclick = function() {
+  document.getElementById('first-section').scrollIntoView({ behavior: 'smooth' });
+};
+document.getElementById('icon-menu').onclick = function() {
+  document.getElementById('section-prods').scrollIntoView({ behavior: 'smooth' });
+};
+document.getElementById('icon-contact').onclick = function() {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+};
+
+// menu alternativo
+
+// BOTON MENUS
+
+document.getElementById('btnProductos').addEventListener('click', function() {
+  var submenu = document.getElementById('submenu');
+  if (submenu.style.display === 'block') {
+      submenu.style.display = 'none';
+  } else {
+      submenu.style.display = 'block';
+  }
+});
+
+document.addEventListener('click', function(event) {
+  var isClickInsideMenu = document.getElementById('menu').contains(event.target);
+  var isClickInsideSubmenu = document.getElementById('submenu').contains(event.target);
+
+  if (!isClickInsideMenu && !isClickInsideSubmenu) {
+      document.getElementById('submenu').style.display = 'none';
+  }
+});
